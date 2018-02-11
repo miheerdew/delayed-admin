9bf6301b0c7a75596cb84abb97e7cb65e624ff99
Miheer Dewaskar <miheerdew@gmail.com>
2018-02-10 22:02:21 -0500

    Implementation using at

--- lib/macos.sh
+++ lib/macos.sh
@@ -4,3 +4,6 @@ function gadd { dseditgroup -o create "$1"; }
 function add_user_to_group { dseditgroup -o edit -a "$1" -t user "$2"; }
 function remove_user_from_group { dseditgroup -o edit -d "$1" -t user "$2"; }
 readonly ADMIN_GROUP="admin"
+
+function check_atd_is_running { launchctl list | grep -q com.apple.atrun; }
+function start_atd { launchctl load -w /System/Library/LaunchDaemons/com.apple.atrun.plist; }

a33d199c34349b5e7941eb0581e2dc6ed4625cd2
Miheer Dewaskar <miheerdew@gmail.com>
2018-02-13 22:42:14 -0500

    Tests to verify install/uninstall
    Move install/uninstall to a function
    Add warning and prevent accidental running using sudo.

--- tests/test_delayed_admin.sh
+++ tests/test_delayed_admin.sh
@@ -1,7 +1,9 @@
-testDelayed() {
-  sudo ./setup.sh install || fail "Installation failed"
+#!/bin/bash
 
-  . lib/utils.sh || fail "Loading failed"
+. lib/utils.sh
+
+testing_the_delay() {
+  check_install
 
   with_sudo add_user_to_group "$USER" delayed-admin || fail "Couldn't add $USER to delayed-admin group"
 
@@ -11,7 +13,23 @@ testDelayed() {
 
   with_delayed add_user_to_group "$USER" "$ADMIN_GROUP" || fail "Couldn't regain sudo access"
 
-  sudo ./setup.sh uninstall || fail "Uninstall not successful"
+  check_uninstall
+}
+
+check_install(){
+  sudo ./setup.sh install || fail "Installation failed"
+  assertTrue 'delayed executable not copied' '[ -x /usr/local/bin/delayed ]'
+  assertFalse 'Config file is writable by regular user' '[ -w /etc/delayed-admin.conf ]'
+  sudo visudo -scqf /etc/sudoers.d/delayed-admin || fail 'Error in the sudoers file'
+  gexists delayed-admin || fail 'delayed-admin group has not been created'
+}
+
+check_uninstall() {
+ sudo ./setup.sh uninstall || fail "Un-installation failed"
+ [ -x /usr/local/bin/delayed ] && fail "Failed to remove delayed binary"
+ [ -f /etc/sudoers.d/delayed-admin.sudoers ] && fail "Failed to remove sudoers"
+ [ -f /etc/delayed-admin.conf ] && fail "Failed to remove config file"
+ gexists delayed-admin && fail "Failed to remove the delayed-admin group"
 }
 
 function with_sudo {
@@ -36,4 +54,29 @@ function check_access_is_revoked {
  #Check if we can execute the true command (under a new login if required)
  ! ( sudo -n true && sudo -n su $USER -c "sudo -n /bin/true" )
 }
+
+
+if [ $EUID -eq 0 ]; then
+  cat >&2 <<_EOF
+Don't run this script as root. You will be asked to provide your sudo credentials later.
+_EOF
+  exit 1
+fi
+
+cat >&2 <<_EOF
+################################################################################
+Attention! This script can mess up your sudo privileges if something goes wrong.
+################################################################################
+
+Either run this script as a new sudo user or keep a root shell running.
+If you lose sudo access because of this script, add yourself back to
+$ADMIN group using the root shell
+
+`declare -f add_user_to_group`
+add_user_to_group $USER $ADMIN_GROUP
+===============================================================================
+_EOF
+
+sudo -K
+
 . tests/shunit2
